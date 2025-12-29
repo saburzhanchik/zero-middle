e421768ee059596cbbe0c16c0f1237b88b3f8cec
Sabyrka <saburzhanchik@gmail.com>
2025-12-30 00:56:22 +0500

    feat: add state management for track list and loading indicators

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import './App.css'
+import { useState } from 'react'
 
-const tracks = [
-  {
-    id: 1,
-    title: 'MusicFun Soundtrack',
-    url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3',
-  },
-  {
-    id: 2,
-    title: 'MusicFun Soundtrack – Instrumental',
-    url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3',
-  },
-]
+type Track = {
+  id: number
+  title: string
+  url: string
+}
 
 export const App = () => {
+  const [tracks, setTracks] = useState<Track[]>([
+    {
+      id: 1,
+      title: 'MusicFun Soundtrack',
+      url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3',
+    },
+    {
+      id: 2,
+      title: 'MusicFun Soundtrack – Instrumental',
+      url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3',
+    },
+  ])
+
   return (
     <>
       <h1>MusicFun Player</h1>
+      {tracks === null && <p>Loading...</p>}
+      {tracks.length === 0 && <p>No tracks</p>}
       <ul>
-        {tracks.map((track) => (
+        {tracks.map((track: Track) => (
           <li key={track.id}>
             <div>{track.title}</div>
             <audio src={track.url} controls></audio>
