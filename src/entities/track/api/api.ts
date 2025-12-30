c4ebd091fccbdc140e435b193ec6f173f2279f28
Sabyrka <saburzhanchik@gmail.com>
2025-12-30 06:03:08 +0500

    feat: refactor API functions to use async/await for improved readability and error handling

--- src/entities/track/api/api.ts
+++ src/entities/track/api/api.ts
@@ -5,14 +5,16 @@ if (apiKey) {
   headers['api-key'] = apiKey
 }
 
-export const getTrack = (trackId: string | null) => {
-  return fetch(`https://musicfun.it-incubator.app/api/1.0/playlists/tracks/${trackId}`, {
+export const getTrack = async (trackId: string | null) => {
+  const res = await fetch(`https://musicfun.it-incubator.app/api/1.0/playlists/tracks/${trackId}`, {
     headers: headers,
-  }).then((res) => res.json())
+  })
+  return await res.json()
 }
 
-export const getTracks = () => {
-  return fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
+export const getTracks = async () => {
+  const res = await fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
     headers: headers,
-  }).then((res) => res.json())
+  })
+  return await res.json()
 }
