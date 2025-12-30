0fe8d50971b6685de8d8fe6281af4c10826dc4fc
Sabyrka <saburzhanchik@gmail.com>
2025-12-30 06:02:48 +0500

    feat: implement dynamic API key handling in fetch requests and add GitHub Actions workflow for deployment

--- src/entities/track/api/api.ts
+++ src/entities/track/api/api.ts
@@ -1,15 +1,18 @@
+const apiKey = import.meta.env.VITE_API_KEY
+const headers: HeadersInit = {}
+
+if (apiKey) {
+  headers['api-key'] = apiKey
+}
+
 export const getTrack = (trackId: string | null) => {
   return fetch(`https://musicfun.it-incubator.app/api/1.0/playlists/tracks/${trackId}`, {
-    headers: {
-      'api-key': import.meta.env.VITE_API_KEY,
-    },
+    headers: headers,
   }).then((res) => res.json())
 }
 
 export const getTracks = () => {
   return fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
-    headers: {
-      'api-key': import.meta.env.VITE_API_KEY,
-    },
+    headers: headers,
   }).then((res) => res.json())
 }
